1ecc187b59e9d2e727b58316d1709a431ef465fe
Matthew Hodges <matthodgess384@gmail.com>
2026-04-15 09:23:28 -0400

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        dp = [1,2]
+
+
+        for i in range(2,n):
+            dp.append(dp[i-1] + dp[i-2])
+
+        print(dp)
+        return dp[-1]
